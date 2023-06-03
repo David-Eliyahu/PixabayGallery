54b072653b00f416a18bc0e7add9a25815c8261b
Alon Shlider <alon.shlider@gmail.com>
2023-06-03 17:10:03 +0300

    Added likes support

--- app/src/main/java/com/shutterfly/pixabaygallery/ui/gallery_list/list_item/GalleryListItem.kt
+++ app/src/main/java/com/shutterfly/pixabaygallery/ui/gallery_list/list_item/GalleryListItem.kt
@@ -1,26 +1,48 @@
 package com.shutterfly.pixabaygallery.ui.gallery_list.list_item
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
 import com.shutterfly.pixabaygallery.models.network_models.GalleryItem
 
 @Composable
-fun GalleryListItem(item: GalleryItem, onClick : (GalleryItem) -> Unit) {
-    AsyncImage(
-        model = item.previewUrl,
-        contentDescription = "",
-        contentScale = ContentScale.FillBounds,
-        modifier = Modifier.padding(2.dp).size(100.dp).clickable {
-            onClick(item)
-        }
-    )
+fun GalleryListItem(item: GalleryItem, onClick: (GalleryItem) -> Unit) {
+    Box(modifier = Modifier.wrapContentSize()) {
+        Text(
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(1.dp).zIndex(1f),
+            fontSize = 10.sp,
+            fontWeight = FontWeight.Bold,
+            text = item.likes.toString()
+        )
+        AsyncImage(
+            model = item.previewUrl,
+            contentDescription = "",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .padding(2.dp)
+                .size(100.dp)
+                .clickable {
+                    onClick(item)
+                }
+        )
+    }
 }
 
 @Preview(showBackground = true)
